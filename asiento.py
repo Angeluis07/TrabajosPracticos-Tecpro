e0f643c6eb4d1a463e3b3d24c01c366678cf1281
Matias G.C. <matigc046@hotmail.com>
2025-04-24 17:17:48 -0300

    Agregue metodos de clases: Asiento y Unidad

--- asiento.py
+++ asiento.py
@@ -2,3 +2,19 @@ class Asiento:
     def __init__(self, numero: int, ocupado: bool):
         self.__numero = numero
         self.__ocupado = ocupado
+        
+    @property
+    def numero(self):
+        return self.__numero
+
+    @numero.setter
+    def numero(self, value):
+        self.__numero = value
+
+    @property
+    def ocupado(self):
+        return self.__ocupado
+
+    @ocupado.setter
+    def ocupado(self, value):
+        self.__ocupado = value
--- unidad.py
+++ unidad.py
@@ -1,2 +1,41 @@
-clase 1
-clase 2
+from asiento import Asiento
+from typing import List 
+class Unidad: 
+    def __init__(self, patente, asientos : List['Asiento']):
+        self.patente = patente
+        self.asientos = asientos if asientos else []
+
+    @property
+    def patente(self):
+        return self._patente
+
+    @patente.setter
+    def patente(self, value):
+        self._patente = value
+
+    @property
+    def asientos(self):
+        return self._asientos
+
+    @asientos.setter
+    def asientos(self, value):
+        self._asientos = value
+
+    #pensar implementacion, pq se pide mostrar asientos disponibles y tambien el listado completo,
+    #yo creo que de esta forma es mejor, que tener dos metodos distintos.
+    def mostrar_asientos(self, x: bool): #Muestro los asientos en forma de lista
+        print("Listado de Asientos")
+        print("  -Disponibles")
+        for asien in self.asientos:
+            if asien.ocupado:
+                print(f"    {asien.numero}")
+        if not x:        
+            print("  -Ocupados")        
+            for asien in self.asientos:
+                if not asien.ocupado:
+                    print(f"    {asien.numero}")
+
+
+    def reservar_asiento(self, asiento: 'Asiento'):
+        pass
+        
