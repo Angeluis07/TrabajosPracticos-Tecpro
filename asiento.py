47d186d344726cfd0abad9b34a47cd67a646b484
JuniorAlba <juninhoalba@gmail.com>
2025-04-24 17:56:12 -0300

    Modifico metodos para mostrar listado de asientos y nombres de atributos para utilizar name-mangling

--- asiento.py
+++ asiento.py
@@ -2,7 +2,7 @@ class Asiento:
     def __init__(self, numero: int, ocupado: bool):
         self.__numero = numero
         self.__ocupado = ocupado
-        
+
     @property
     def numero(self):
         return self.__numero
@@ -18,3 +18,11 @@ class Asiento:
     @ocupado.setter
     def ocupado(self, value):
         self.__ocupado = value
+
+    def mostrar_si_ocupado(self):
+        if self.__ocupado:
+            print(f"El asiento {self.__numero} está ocupado.")
+
+    def mostrar_si_disponible(self):
+        if not self.__ocupado:
+            print(f"El asiento {self.__numero} está disponible.")
--- unidad.py
+++ unidad.py
@@ -1,41 +1,41 @@
 from asiento import Asiento
-from typing import List 
-class Unidad: 
-    def __init__(self, patente, asientos : List['Asiento']):
-        self.patente = patente
-        self.asientos = asientos if asientos else []
+from typing import List
+class Unidad:
+    def __init__(self, patente, asientos : List['Asiento'] = None):
+        self.__patente = patente
+        self.__asientos = asientos if asientos else []
 
     @property
     def patente(self):
-        return self._patente
+        return self.__patente
 
     @patente.setter
     def patente(self, value):
-        self._patente = value
+        self.__patente = value
 
     @property
     def asientos(self):
-        return self._asientos
+        return self.__asientos
 
     @asientos.setter
     def asientos(self, value):
-        self._asientos = value
+        self.__asientos.append(value)
 
     #pensar implementacion, pq se pide mostrar asientos disponibles y tambien el listado completo,
     #yo creo que de esta forma es mejor, que tener dos metodos distintos.
-    def mostrar_asientos(self, x: bool): #Muestro los asientos en forma de lista
+    #Revisar si cumple con tell dont ask
+    def mostrar_asientos_disponibles(self): #Muestro los asientos en forma de lista
         print("Listado de Asientos")
         print("  -Disponibles")
-        for asien in self.asientos:
-            if asien.ocupado:
-                print(f"    {asien.numero}")
-        if not x:        
-            print("  -Ocupados")        
-            for asien in self.asientos:
-                if not asien.ocupado:
-                    print(f"    {asien.numero}")
+        for asiento in self.__asientos:
+            print(f"    {asiento.mostrar_si_disponible()}")
+
+    def mostrar_asientos_ocupados(self):
+        print("Listado de Asientos")
+        print("  -Ocupados")
+        for asiento in self.__asientos:
+            print(f"    {asiento.mostrar_si_ocupado()}")
 
 
     def reservar_asiento(self, asiento: 'Asiento'):
         pass
-        
