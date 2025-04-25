380d6c8a165d2df219117bf0fb2100bb63adf762
Matias G.C. <matigc046@hotmail.com>
2025-04-25 15:28:54 -0300

    Agrego metodos de procesar pagos para los distintos metodos de pago

--- medios_pagos.py
+++ medios_pagos.py
@@ -9,12 +9,13 @@ class MedioPago(ABC):
         pass
 
 class TarjetaCredito(ABC):
-    def __init__(self, numero: str, DNI: int, nombre: str, fecha_vencimiento: date):
+    def __init__(self, numero: str, dni: int, nombre: str, fecha_vencimiento: date):
         super().__init__()
-        self.__numero = numero 
-        self.__DNI = DNI
+        self.__numero = numero
+        self.__dni = dni    #DNI del titular de la tarjeta
         self.__nombre = nombre
         self.__fecha_nacimiento = fecha_vencimiento
+
     @property
     def numero(self):
         return self.__numero
@@ -24,12 +25,14 @@ class TarjetaCredito(ABC):
         self.__numero = value
 
     @property
-    def DNI(self):
-        return self.__DNI
+    def dni(self):
+        return self.__dni
 
-    @DNI.setter
-    def DNI(self, value):
-        self.__DNI = value
+    @dni.setter
+    def dni(self, value):
+        if not isinstance(value, int):
+            raise ValueError("DNI debe ser un número entero.")
+        self.__dni = value
 
     @property
     def nombre(self):
@@ -46,7 +49,18 @@ class TarjetaCredito(ABC):
     @fecha_vencimiento.setter
     def fecha_vencimiento(self, value):
         self.__fecha_nacimiento = value
-    
+
+    #Simulacion del proceso de pago
+    def procesar_pago(self, monto: float)->bool:
+        estado = True
+        if self.fecha_vencimiento > date.today():
+            print(f"Pago de ${monto} procesado con tarjeta {self.numero}.")
+        else:
+            print(f"Pago de ${monto} no procesado. La tarjeta {self.numero} está vencida.")
+            estado = False
+        return estado
+
+
 class MercadoPago(MedioPago):
     def __init__(self, celular: str, email: str):
         super().__init__()
@@ -67,6 +81,16 @@ class MercadoPago(MedioPago):
     @email.setter
     def email(self, value):
         self.__email = value
+
+    def procesar_pago(self, monto: float)->bool:
+        #Simulacion del proceso de pago
+        estado = True
+        if self.celular and self.email:
+            print(f"Pago de ${monto} procesado con MercadoPago.")
+        else:
+            print(f"Pago de ${monto} no procesado. Datos de MercadoPago incompletos.")
+            estado = False
+        return estado
 
 class UALA(MedioPago):
     def __init__(self, email: str, nombre_titular: str):
@@ -89,3 +113,14 @@ class UALA(MedioPago):
     @nombre_titular.setter
     def nombre_titular(self, value):
         self.__nombre_titular = value
+
+    def procesar_pago(self, monto: float)->bool:
+        #Simulacion del proceso de pago
+        #Tambien que tenga en cuenta el size minimo que debe poder tener el mail y el siz emaximo
+        estado = True
+        if self.email and self.nombre_titular:
+            print(f"Pago de ${monto} procesado con UALA.")
+        else:
+            print(f"Pago de ${monto} no procesado. Datos de UALA incompletos.")
+            estado = False
+        return estado
