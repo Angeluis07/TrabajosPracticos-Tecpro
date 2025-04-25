089bc2c7e70614d7d7400ce49562edf243cf4684
Palacios Angel Luis <119541688+Angeluis07@users.noreply.github.com>
2025-04-25 12:05:06 -0300

    Implemento las clases Ciudad e Itinerario

--- ciudad.py
+++ ciudad.py
@@ -3,3 +3,27 @@ class Ciudad:
         self.__codigo = codigo
         self.__nombre = nombre
         self.__provincia = provincia
+
+    @property
+    def codigo(self):
+        return self.__codigo
+
+    @codigo.setter
+    def codigo(self, value):
+        self.__codigo = value
+
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @nombre.setter
+    def nombre(self, value):
+        self.__nombre = value
+
+    @property
+    def provincia(self):
+        return self.__provincia
+
+    @provincia.setter
+    def provincia(self, value):
+        self.__provincia = value
---
+++ itinerario.py
@@ -0,0 +1,42 @@
+from ciudad import Ciudad
+
+class Itinerario:
+    def __init__(self, codigo: str, ciudad_origen: Ciudad, ciudad_destino: Ciudad, paradas_intermedias: list[Ciudad]):
+        self.__codigo = codigo
+        self.__ciudad_origen = ciudad_origen
+        self.__ciudad_destino = ciudad_destino
+        self.__paradas_intermedias = paradas_intermedias
+
+    @property
+    def codigo(self):
+        return self.__codigo
+
+    @codigo.setter
+    def codigo(self, value):
+        self.__codigo = value
+
+    @property
+    def ciudad_origen(self):
+        return self.__ciudad_origen
+
+    @property
+    def ciudad_destino(self):
+        return self.__ciudad_destino
+
+    @property
+    def paradas_intermedias(self) -> list[Ciudad]:
+        return self.__paradas_intermedias
+
+    def __str__(self) -> str:
+        return f"Codigo: {self.__codigo}, Ciudad Origen: {self.__ciudad_origen.nombre}, Ciudad Destino: {self.__ciudad_destino.nombre}, Paradas Intermedias: {[ciudad.nombre for ciudad in self.__paradas_intermedias]}"
+
+
+if __name__ == '__main__':
+    ciudad1 = Ciudad("001", "Madrid", "Madrid")
+    ciudad2 = Ciudad("002", "Barcelona", "Cataluña")
+    ciudad3 = Ciudad("003", "Valencia", "Comunidad Valenciana")
+    ciudad4 = Ciudad("004", "Sevilla", "Andalucía")
+    ciudad5 = Ciudad("005", "Bilbao", "País Vasco")
+
+    itinerario = Itinerario("IT001", ciudad1, ciudad2, [ciudad3, ciudad4, ciudad5])
+    print(itinerario)
