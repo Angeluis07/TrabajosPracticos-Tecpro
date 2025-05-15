b650d7f9120ad2f9c740204569a289d07c835895
JuniorAlba <juninhoalba@gmail.com>
2025-05-15 19:31:00 -0300

    Implemento metodos necesarios para que la clase argentina_tur funcione, no esta terminado

--- tp_funcional/tp.rkt
+++ tp_funcional/tp.rkt
@@ -15,32 +15,66 @@
 
 
 
-(define localidades '("Buenos Aires" "Córdoba" "Rosario" "Mendoza" "Salta"))
+(define (buscar_detalle_localidad localidad)
+  (letrec ([buscar-localidad (lambda (lst)
+                               (cond
+                                 [(null? lst) "Error"]
+                                 [(equal? (car (car lst)) localidad) (car lst)]
+                                 [else (buscar-localidad (cdr lst))]))])
+    (buscar-localidad itinerario)))
 
 
 ;2) Obtener la lista de horarios de una localidad
 (define (horarios localidad)
- (letrec ([buscar-horarios (lambda (lst)
-                            (cond
-                              ; El condicional cond permite que se evaluen estas 3 condiciones 
-                              [(null? lst) "Error"]
-                              [(equal? (car (car lst)) localidad) (caddr (car lst))]
-                              [else (buscar-horarios (cdr lst))]))])
-      (buscar-horarios itinerario)))
-  
+ (car(cdr(cdr (buscar_detalle_localidad localidad)))))
 
-;3) Obtener precio de una localidad
-(define (precio localidad)
- (letrec ([buscar-precio (lambda (lst)
-                            (cond
-                              ; El condicional cond permite que se evaluen estas 3 condiciones 
-                              [(null? lst) "Error"]
-                              [(equal? (car (car lst)) localidad) (cadr (car lst))]
-                              [else (buscar-precio (cdr lst))]))])
-      (buscar-precio itinerario)))
+(define (hora_valida hora_consulta hora_salida)
+  (cond
+    [(< (car hora_consulta) (car hora_salida)) #t]
+    [(and (= (car hora_consulta) (car hora_salida)) (<= (cadr hora_consulta) (cadr hora_salida))) #t]
+    [else #f]))
 
 
 ;3) Funcion que recibe dos localidades y devuelve el un subconjuto con localidad 1
+(define (subconjunto localidad1 localidad2)
+  (letrec ([buscar-subconjunto (lambda (lst)
+                               (cond
+                                 [(null? lst) "ERROR"]
+                                 [(equal? (car (car lst)) localidad2) "ERROR"]
+                                 [(equal? (car (car lst)) localidad1) lst]
+                                 [else (buscar-subconjunto (cdr lst))]))])
+    (buscar-subconjunto itinerario)))
+
+(define (precio_viaje localidad1 localidad2)
+  (if (equal? (subconjunto localidad1 localidad2) "ERROR")
+      "ERROR"
+      (letrec ([precio (lambda (lst)
+                         (cond
+                           [(null? lst) "ERROR"]
+                           [(equal? (car (car lst)) localidad2) 0]
+                           [else (+ (car (cdr (car lst))) (precio (cdr lst)))]))])
+        (precio (subconjunto localidad1 localidad2)))))
+
+(define (verificar_horario hora_consulta localidad)
+  (define horas_disponibles (horarios localidad))
+  (letrec ([resultado (lambda (lst1)
+                         (cond
+                           [(hora_valida hora_consulta car(lst1)) lst1]
+                           [(null? lst1) '()]
+                           [else (resultado (cdr lst1))]))])
+        (resultado (horas_disponibles))))
+
+
+(define (ArgentinaTur lista_consulta)
+  (define resultado (precio_viaje car(lista_consulta) car(cdr(lista_consulta))))
+  (if (equal? resultado  "ERROR")
+    "ERROR"
+    (if (equal? (verificar_horario (car(cdr(cdr lista_consulta)))) '() )
+      (list (list car(lista_consulta) car(cdr(lista_consulta))) 0 "NO HAY HORARIOS DE SALIDA DISPONIBLES")
+      (list (list car(lista_consulta) car(cdr(lista_consulta))) resultado (verificar_horario (car(cdr(cdr lista_consulta))) car(lista_consulta))))))
+
+
+
 
 
 ;Wrapper ArgentinaTur
